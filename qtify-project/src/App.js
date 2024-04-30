e06e401a3c9f7c497221024e32d0e1c1ca03eb6f
Likhith <likhithsai7979@gmail.com>
2024-04-30 13:07:25 +0530

    checking

--- qtify-project/src/App.js
+++ qtify-project/src/App.js
@@ -1,6 +1,6 @@
 // import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
-// import Hero from './components/Hero/Hero.jsx'
+import Hero from './components/Hero/Hero.jsx'
 // import styles from './App.module.css'
 // import { useEffect, useState } from 'react'
 // import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from "./api/api"
@@ -120,15 +120,15 @@ function App() {
   
   
   return (
-//     <>
+   <>
    <Navbar />
-//       <Hero />
-//       <div className={styles.sectionWrapper}>
-//         <Section type='album' title='Top Albums' data={topAlbumSongs} />
-//         <Section type='album' title='New Albums' data={newAlbumSongs} />
-//         <FilterSection data={newAlbumSongs} type='songFilter' title='Songs' filteredData={filteredData} filteredDataValues={filteredDataValues} value={value} handleChange={handleChange} handleToggle={handleToggle}/>
-//       </div>
-//     </>
+      <Hero />
+     {/* <div className={styles.sectionWrapper}> */}
+       {/* <Section type='album' title='Top Albums' data={topAlbumSongs} />
+      <Section type='album' title='New Albums' data={newAlbumSongs} />
+         <FilterSection data={newAlbumSongs} type='songFilter' title='Songs' filteredData={filteredData} filteredDataValues={filteredDataValues} value={value} handleChange={handleChange} handleToggle={handleToggle}/> */}
+      {/* </div> */}
+     </>
  )
 }
 
